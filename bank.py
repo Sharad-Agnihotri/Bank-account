72b7b784b1ee7d6c97e1eaac038ecb1030e03be9
Sharad-Agnihotri <sharadagnihotri197@gmail.com>
2025-10-06 17:03:38 +0530

    Save to withdraw-feature

--- bank.py
+++ bank.py
@@ -9,3 +9,26 @@ new_balance = initial_balance + deposit
 print("Initial Balance:", initial_balance)
 print("Deposit:", deposit)
 print("New Balance after deposit:",new_balance)
+
+
+
+# Ask user for balance, deposit, and withdrawal
+initial_balance = float(input("Enter Initial Balance: "))
+deposit = float(input("Enter Deposit Amount: "))
+
+# Calculate balance after deposit
+balance_after_deposit = initial_balance + deposit
+print("Initial Balance:", initial_balance)
+print("Deposit:", deposit)
+print("New Balance after deposit:", balance_after_deposit)
+
+# Withdrawal feature
+withdraw = float(input("Enter Withdrawal Amount: "))
+
+# Check if sufficient funds
+if withdraw <= balance_after_deposit:
+    new_balance = balance_after_deposit - withdraw
+    print("Amount Withdrawn:", withdraw)
+    print("New Balance after withdrawal:", new_balance)
+else:
+    print("Insufficient balance!")
